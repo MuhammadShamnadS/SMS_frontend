6fa3f1d45fc79685fc11c54ee4c268e0d18ae0e3
MuhammadShamnadS <219637101+MuhammadShamnadS@users.noreply.github.com>
2025-08-20 18:07:48 +0530

    fix-updated socket creation

--- src/realtime/echo.js
+++ src/realtime/echo.js
@@ -3,10 +3,11 @@ import Echo from "laravel-echo";
 import Pusher from "pusher-js"; // required by Echo, even with Reverb
 window.Pusher = Pusher;
 
-export default function makeEcho() {
+let echoInstance = null;
+export default function getEcho() {
+  if (echoInstance) return echoInstance;
   const token = localStorage.getItem("accessToken");
-
-  return new Echo({
+  echoInstance = new Echo({
     broadcaster: "reverb",
     key: import.meta.env.VITE_REVERB_APP_KEY || "local-key",
     wsHost: import.meta.env.VITE_REVERB_HOST || window.location.hostname,
@@ -18,4 +19,5 @@ export default function makeEcho() {
       headers: { Authorization: `Bearer ${token}` }, // ← JWT to pass your api guard
     },
   });
+  return echoInstance;
 }
